a542998bcfe627423f698d0e2d6a05d6a65db7c8
Klaas Van Waesberghe <klaasvw@gmail.com>
2014-09-03 12:03:56 +0200

    Added pushstream test script

---
+++ test/client/execution_pipeline.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josbert
+ * Date: 31/07/14
+ * Time: 10:24
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPConnection;
+
+$connection = new AMQPConnection('msgb.lissa.crosscheck.bev', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('content_notification', false, false, false, false);
+
+echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+
+$ch = curl_init('http://worker.lissa.crosscheck.be/publish?channel=1');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$counter = 0;
+$callback = function($msg) {
+  global $ch;
+  static $counter = 0;
+  $counter++;
+
+  $data = array(
+    'id' => $counter,
+    'text' => $msg->body);
+  $json_data = json_encode($data);
+
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+  $ret = curl_exec($ch);
+};
+
+$channel->basic_consume('content_notification', '', false, true, false, false, $callback);
+
+while(count($channel->callbacks)) {
+  $channel->wait();
+}
+
+curl_close($ch);
