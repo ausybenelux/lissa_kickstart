5e96b32344f7502ca6280018594d333c319cf4af
Klaas Van Waesberghe <klaasvw@gmail.com>
2014-10-08 10:01:30 +0200

    Issue #LISSA-102: Redirect operators and production to events overview.

--- modules/lissa_kickstart/lissa_kickstart_core/src/Controller/LissaKickstartCoreController.php
+++ modules/lissa_kickstart/lissa_kickstart_core/src/Controller/LissaKickstartCoreController.php
@@ -8,6 +8,7 @@
 namespace Drupal\lissa_kickstart_core\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\user\Entity\User;
 
 /**
  * Controller routings for lissa_kickstart_core routes.
@@ -20,7 +21,16 @@ class LissaKickstartCoreController extends ControllerBase {
    * This controller will show the user login page when not logged in.
    */
   public function home() {
-    $response = $this->redirect('user.page');
+    $roles = $this->currentUser()->getRoles();
+
+    // Show the events overview when the user has production or operator access.
+    if (in_array('production', $roles) || in_array('operator', $roles)) {
+      $response = $this->redirect('events.admin_overview');
+    }
+    // If user is anonymous or has no manage event access.
+    else {
+      $response = $this->redirect('user.page');
+    }
     return $response;
   }
 }
