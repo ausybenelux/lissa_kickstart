c9d2c5c268b13c9ef302de3064b51fecf55a9498
Stef Trenson <stef.trenson@dotprojects.be>
2014-10-09 11:55:00 +0200

    Issue #LISSA-48: Reformat code

--- modules/lissa/notification_entity/src/NotificationEntityForm.php
+++ modules/lissa/notification_entity/src/NotificationEntityForm.php
@@ -15,28 +15,30 @@ use Drupal\Core\Language\LanguageInterface;
  * Adds the image file field to the NotificationEntity form
  */
 class NotificationEntityForm extends ContentEntityForm {
-    /**
-     * {@inheritdoc}
-     */
-    public function form(array $form, FormStateInterface $form_state) {
-        $form = parent::form($form, $form_state);
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array $form, FormStateInterface $form_state)
+  {
+    $form = parent::form($form, $form_state);
 
-        // Let the user upload an image
-        $form['image_id'] = [
-            '#type' => 'managed_file',
-            '#name' => 'image',
-            '#title' => $this->t('Image'),
-        ];
+    // Let the user upload an image
+    $form['image_id'] = [
+      '#type' => 'managed_file',
+      '#name' => 'image',
+      '#title' => $this->t('Image'),
+    ];
 
-        return $form;
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submit()
+  {
+    if (isset($form_state['values']['custom_content_block_image'])) {
+      file_save_data($form_state['values']['custom_content_block_image']);
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function submit() {
-        if (isset($form_state['values']['custom_content_block_image'])) {
-            file_save_data($form_state['values']['custom_content_block_image']);
-        }
-    }
+  }
 }
