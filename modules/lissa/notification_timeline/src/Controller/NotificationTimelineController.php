5dc0f6512dc992ec59b1adff7196427c72a68c7e
Klaas Van Waesberghe <klaasvw@gmail.com>
2014-10-09 18:11:41 +0200

    Issue #LISSA-54: Added timeline proof of concept flow.

--- modules/lissa/notification_timeline/src/Controller/NotificationTimelineController.php
+++ modules/lissa/notification_timeline/src/Controller/NotificationTimelineController.php
@@ -9,6 +9,7 @@ namespace Drupal\notification_timeline\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\NodeInterface;
+use Drupal\taxonomy\VocabularyInterface;
 
 
 /**
@@ -17,6 +18,16 @@ use Drupal\node\NodeInterface;
 class NotificationTimelineController extends ControllerBase {
 
   public function nodeLoad(NodeInterface $node) {
-    return 'Timeline';
+    $build = array();
+    $build['notification_form'] = $this->formBuilder()->getForm('Drupal\notification_timeline\Form\NotificationTimelineNotificationForm');
+    return $build;
+  }
+
+  public function addNotification(NodeInterface $node, $vocabulary) {
+    $build = array();
+    $form_builder = \Drupal::service('entity.form_builder');
+    $entity = \Drupal::entityManager()->getStorage('taxonomy_term')->create(array('vid' => $vocabulary));
+    $build['notification_form'] = $form_builder->getForm($entity);
+    return $build;
   }
 }
---
+++ modules/lissa/notification_timeline/src/Form/NotificationTimelineNotificationForm.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @file
+ *
+ * Contains \Drupal\notification_timeline\Form\NotificationTimelineNotificationForm.
+ */
+
+namespace Drupal\notification_timeline\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class NotificationTimelineNotificationForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'notification_timeline_notification_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['notification_timeline'] = array(
+      '#tree' => TRUE,
+    );
+
+    // Build a list of notification types.
+    $options = array('0' => t('- Pick Event Type -'));
+    foreach (\Drupal::entityManager()->getStorage('taxonomy_vocabulary')->loadMultiple() as $type) {
+      $options[$type->id()] = $type->label();
+    }
+
+    $form['notification_timeline']['notification_type'] = array(
+      '#type' => 'select',
+      '#options' => $options,
+    );
+
+    $form['add'] = array(
+      '#type' => 'submit',
+      '#name' => 'add',
+      '#value' => t('Add'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $notification_type = $form_state->getValue(array('notification_timeline', 'notification_type'));
+    $node = \Drupal::request()->attributes->get('node');
+    $form_state->setRedirect('notification_timeline.notification_form', array(
+      'node' => $node->id(),
+      'vocabulary' => $notification_type,
+    ));
+  }
+}
