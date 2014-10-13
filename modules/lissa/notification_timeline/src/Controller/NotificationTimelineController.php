5fe05eceb047f080d9afdd7c440abdb9fa4eb56f
Klaas Van Waesberghe <klaasvw@gmail.com>
2014-10-13 10:26:57 +0200

    Issue #LISSA-54: Added notification list to nodes.

--- modules/lissa/notification_timeline/src/Controller/NotificationTimelineController.php
+++ modules/lissa/notification_timeline/src/Controller/NotificationTimelineController.php
@@ -17,17 +17,50 @@ use Drupal\taxonomy\VocabularyInterface;
  */
 class NotificationTimelineController extends ControllerBase {
 
+  /**
+   * Route page callback: returns the HTML for the default timeline page.
+   *
+   * @param NodeInterface $node
+   *   The node to show the timeline for.
+   *
+   * @return array
+   *   A render array containing the data to display.
+   */
   public function nodeLoad(NodeInterface $node) {
     $build = array();
     $build['notification_form'] = $this->formBuilder()->getForm('Drupal\notification_timeline\Form\NotificationTimelineNotificationForm');
     return $build;
   }
 
+  /**
+   * Route page callback: returns the HTML for adding a notification
+   *
+   * @param NodeInterface $node
+   *   The node to add a notification to.
+   * @param string $notification_type
+   *   The id of the notification type.
+   *
+   * @return array
+   *   A render array containing the data to display.
+   */
   public function addNotification(NodeInterface $node, $notification_type) {
     $build = array();
     $form_builder = \Drupal::service('entity.form_builder');
     $entity = \Drupal::entityManager()->getStorage('notification_entity')->create(array('type' => $notification_type));
     $build['notification_form'] = $form_builder->getForm($entity);
+    $entities = $this->getNotifications($node);
+    $build['timeline'] = \Drupal::entityManager()->getViewBuilder('notification_entity')->viewMultiple($entities, 'full');
     return $build;
+  }
+
+  /**
+   * Returns an array of notifications linked to the specified node.
+   */
+  protected function getNotifications(NodeInterface $node) {
+    $entity_query = \Drupal::entityQuery('notification_entity', 'AND');
+    $entity_query->condition('host_id', $node->id());
+    $entity_query->sort('timeline', 'DESC');
+    $result = $entity_query->execute();
+    return $result ? $this->loadMultiple($result) : array();
   }
 }
