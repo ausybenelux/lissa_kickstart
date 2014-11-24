12d4ff1d04f054beaf2eba73aed5bce7e7f7272d
Klaas Van Waesberghe <klaasvw@gmail.com>
2014-11-24 11:00:16 +0100

    Issue #LISSA-108: Added active and staging config dirs to configuration.

--- default.settings.php
+++ default.settings.php
@@ -1,9 +1,17 @@
 <?php
 
-$config_directories= array();
+// The active and stage directories need to be defined here because the install
+// phase will attempt to search for them if there are one or more custom config
+// directories.
+$config_base_directory = DRUPAL_ROOT . '/profiles/lissa_kickstart/config';
 
-if (file_exists(DRUPAL_ROOT . '/profiles/lissa_kickstart/config/deploy')) {
-  $config_directories['deploy'] = 'profiles/lissa_kickstart/config/deploy';
+$config_directories = array(
+  CONFIG_ACTIVE_DIRECTORY => $config_base_directory . '/active',
+  CONFIG_STAGING_DIRECTORY => $config_base_directory . '/staging',
+);
+
+if (file_exists($config_base_directory . '/deploy')) {
+  $config_directories['deploy'] = $config_base_directory . '/deploy';
 }
 
 $settings['hash_salt'] = 'NVPgQ-GudJktovJTq5v7x-3FveMR9dxdjsf6OOFxCl2Omd7iS2tFG4Ldt68dZ3yBpEyVdCgyGA';
