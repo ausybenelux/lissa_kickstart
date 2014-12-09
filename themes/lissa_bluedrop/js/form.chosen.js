b696f8e1151e4c8c66f3992a9184408d2089445b
Klaas Van Waesberghe <klaasvw@gmail.com>
2014-12-09 16:49:59 +0100

    Issue -118: Use chosen

--- themes/lissa_bluedrop/js/form.chosen.js
+++ themes/lissa_bluedrop/js/form.chosen.js
@@ -1,18 +1,19 @@
 /**
  * @file
- * Defines Javascript behaviors for Selectize.
+ * Defines Javascript behaviors for Chosen.
  */
 
 (function ($, Drupal, drupalSettings) {
 
   "use strict";
 
-  Drupal.behaviors.selectize = {
+  Drupal.behaviors.chosen = {
     attach: function (context) {
-      $(context).find('select').once('selectize').selectize({
-
+      $(context).find('#notification-forms select').once('chosen').chosen({
+        width: '300px'
       });
-    }
+    },
+    weight: 100
   };
 
 })(jQuery, Drupal, drupalSettings);
